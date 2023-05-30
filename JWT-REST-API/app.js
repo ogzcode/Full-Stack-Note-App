060e9ff68e184ea9d8e772217aeea087f48fb444
ogzcode <oguzguc44@gmail.com>
2023-05-30 17:50:33 +0300

    Added Delete and UpdateProfile routes and controller

--- JWT-REST-API/app.js
+++ JWT-REST-API/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 
 import { auth } from "./auth.js";
-import { getTest, register, login, getAuthUser, addNote, getUserNote, deleteNote } from "./controllers/apiControllers.js";
+import { getTest, addNote, getUserNote, deleteNote, updateProfile } from "./controllers/apiControllers.js";
+import { register, login, getAuthUser, deleteUser} from "./controllers/authController.js";
 
 const app = express();
 
@@ -16,10 +17,13 @@ app.get("/test", getTest);
 app.post("/register", register);
 app.post("/login", login);
 
+app.delete("/auth/deleteUser", auth, deleteUser);
+
 app.get("/auth", auth, getAuthUser);
 app.post("/auth/addNote", auth, addNote);
 app.get("/auth/notes", auth, getUserNote);
 app.delete("/auth/delete", auth, deleteNote);
+app.patch("/auth/updateProfile", auth, updateProfile);
 
 app.listen(3000, () => {
     console.log("Sunucu: localhost:3000");
--- JWT-REST-API/controllers/apiControllers.js
+++ JWT-REST-API/controllers/apiControllers.js
@@ -1,93 +1,8 @@
-import jwt from "jsonwebtoken";
-import { nanoid } from "nanoid";
 import { readFileSync, writeFileSync } from "fs";
 
 export const getTest = (req, res) => {
     res.send("Hello test");
 };
-
-export const register = (req, res) => {
-    const { email, password } = req.body;
-
-    let users = JSON.parse(readFileSync("./model/users.json")).users;
-
-    if (!(email && password)) {
-        res.status(400).send("All input is required");
-    }
-
-    //if new user exist
-    const oldUser = users.find(user => user.email === email);
-    if (oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
-    }
-
-    //create new user
-    const user = {
-        "id": nanoid(),
-        "email": email,
-        "password": password,
-        "notes": []
-    };
-
-    // Create token
-    const token = jwt.sign(
-        { user_id: user.id, email },
-        "RANDOM-TOKEN",
-        {
-            expiresIn: "1h",
-        }
-    );
-    // save user token
-    user.token = token;
-
-    users.push(user);
-
-    writeFileSync(
-        "./model/users.json",
-        JSON.stringify({ users: users }, null, 2),
-        "utf8"
-    );
-
-    // return new user
-    res.status(201).json({ message: "User added" });
-};
-
-export const login = (req, res) => {
-    let users = JSON.parse(readFileSync("./model/users.json")).users;
-
-    let user = users.find(user => user.email === req.body.email);
-
-    if (!user) {
-        res.status(404).send({ message: "Email not found" });
-    }
-
-    if (user.password !== req.body.password) {
-        res.status(400).send({
-            message: "Passwords does not match",
-        });
-    }
-
-    const token = jwt.sign(
-        {
-            userId: user.id,
-            userEmail: user.email,
-        },
-        "RANDOM-TOKEN",
-        { expiresIn: "1h" }
-    );
-
-    res.status(200).send({
-        message: "Login Successful",
-        email: user.email,
-        token,
-    });
-};
-
-export const getAuthUser = (req, res) => {
-    let users = JSON.parse(readFileSync("./model/users.json")).users;
-    let user = users.find(user => user.email === req.user.userEmail);
-    res.json({ message: "You are authorized to access me", user });
-}
 
 export const addNote = (req, res) => {
     let users = JSON.parse(readFileSync("./model/users.json")).users;
@@ -119,7 +34,7 @@ export const getUserNote = (req, res) => {
 export const deleteNote = (req, res) => {
     let users = JSON.parse(readFileSync("./model/users.json")).users;
     let user = users.find(user => user.email === req.user.userEmail);
-    console.log("hello");
+
     user.notes.splice(req.body.index, 1);
 
     writeFileSync(
@@ -130,3 +45,24 @@ export const deleteNote = (req, res) => {
 
     res.status(204).send({ message: "Deleted"});
 }
+
+export const updateProfile = (req, res) => {
+    let users = JSON.parse(readFileSync("./model/users.json")).users;
+    let user = users.find(user => user.email === req.user.userEmail);
+
+    console.log(req.body);
+    user.firstName = req.body.firstName || user.firstName;
+    user.lastName = req.body.lastName || user.lastName;
+    user.password = req.body.newPassword || user.password;
+
+    console.log(user.firstName, user.lastName, user.password);
+
+    writeFileSync(
+        "./model/users.json",
+        JSON.stringify({ users: users }, null, 2),
+        "utf8"
+    );
+
+    res.status(200);
+};
+
