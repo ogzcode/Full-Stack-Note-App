b8e943b6ab4ddcb719be7d4e1d151f1deab30d11
Oğuzhan Güç <oguzguc44@gmail.com>
2023-06-23 15:28:11 +0300

    -Interceptors have been written for the Bearer token. -Thunk funk for registration and login. written

--- JWT-Frontend/src/services/request.js
+++ JWT-Frontend/src/services/request.js
@@ -1,6 +1,20 @@
 import axios from 'axios';
+import { getToken } from './storage';
 
 const BASE_URL = 'http://localhost:3000';
+
+axios.interceptors.request.use(
+    (config) => {
+        const token = getToken();
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export const registerServices = (data) => {
     return axios.post(`${BASE_URL}/register`, data);
