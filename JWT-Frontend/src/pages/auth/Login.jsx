d37da7eaae7d26375508fab1234da3174b814cb6
Oğuzhan Güç <oguzguc44@gmail.com>
2023-06-26 09:08:07 +0300

    Registration and Login pages reorganized

--- JWT-Frontend/src/pages/auth/Login.jsx
+++ JWT-Frontend/src/pages/auth/Login.jsx
@@ -1,15 +1,20 @@
-import { Form, Button, Alert } from "react-bootstrap";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import { useDispatch, useSelector } from "react-redux";
 import { loginThunk, setError, setErrorMessage } from "../../redux/slice/userSlice";
+
+import loginSVG from "../../assets/login.svg";
+import { AiOutlineArrowRight, AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+import { Alert } from "react-bootstrap";
+
 
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [show, setShow] = useState(false);
-    const [validated, setValidated] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
     const error = useSelector(state => state.user.error);
     const errorMessage = useSelector(state => state.user.errorMessage);
@@ -36,60 +41,61 @@ export default function Login() {
         event.stopPropagation();
 
         if (email && password) {
-            
+
             const response = await dispatch(loginThunk({ email, password }));
 
-            if (errorMessage === null) {
+            if (response.payload?.status === 200) {
                 navigate("/home");
             }
 
             setEmail("");
             setPassword("");
         }
+    };
 
-        setValidated(true);
+    const handleChange = (val, setVal) => {
+        setVal(val);
     };
 
     return (
-        <div className="w-100 d-flex justify-content-center align-items-center flex-column position-relative" style={{ height: "100vh" }}>
-            {
-                errorMessage && <Alert show={show} variant={"danger"} className="position-absolute top-0 start-50 translate-middle-x mt-4">
-                    {errorMessage}
-                </Alert>
-            }
-            <h2 className="mb-3" style={{ fontSize: "36px" }}>Login</h2>
-            <Form className="w-25" onSubmit={handleSubmit} noValidate validated={validated}>
-                {/* email */}
-                <Form.Group controlId="formBasicEmailLogin" className="mb-3">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" name="email" 
-                    value={email} onChange={e => setEmail(e.target.value)} required/>
-                    <Form.Control.Feedback type="invalid">
-                        Please write your email
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-                {/* password */}
-                <Form.Group controlId="formBasicPasswordLogin">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" name="password" 
-                    value={password} onChange={e => setPassword(e.target.value)} required/>
-                    <Form.Control.Feedback type="invalid">
-                        Please write your password
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-                <div className="text-center mt-1">
-                    <button type="button" className="btn btn-link" style={{ fontSize: "14px" }} onClick={() => navigate("/")}>Go to register page</button>
+        <div style={{ height: "100vh" }} className="auth-page relative min-w-full flex justify-center items-center flex-col shadow-lg" >
+            <Alert show={show} errorMessage={errorMessage} />
+            <div className=" flex bg-white px-12 py-20 rounded-lg">
+                <div className="mr-20">
+                    <img src={loginSVG} alt="register" className="w-80 h-full my-auto" />
                 </div>
+                <div className=" w-80">
+                    <h2 className="mb-4 font-semibold text-2xl text-teal-950">Login</h2>
+                    <form className="" onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <label className="block text-sm mb-1 text-slate-500">Email</label>
+                            <input type="email" placeholder="Enter email" name="email" className="text-teal-950 px-2 py-1 rounded-md border w-full border-gray-600 outline-0"
+                                value={email} onChange={e => handleChange(e.target.value, setEmail)} required />
+                        </div>
 
-                {/* submit button */}
-                <div className="text-center mt-3">
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
+                        <div className="mb-3">
+                            <label className="block text-sm mb-1 text-slate-500">Password</label>
+                            <div className="relative">
+                                <input type={showPassword ? "text" : "password"} placeholder="Password" name="password" className="text-teal-950 px-2 py-1 rounded-md border w-full border-gray-600 outline-0"
+                                    value={password} onChange={e => handleChange(e.target.value, setPassword)} required />
+                                <span className="absolute top-1/2 right-2 text-lg text-teal-800 transform -translate-y-1/2 cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
+                                    {showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="text-center">
+                            <Link className="text-sm text-teal-900" to={"/register"}>Go to register page <AiOutlineArrowRight className="inline" /></Link>
+                        </div>
+                        {/* submit button */}
+                        <div className="text-center mt-8">
+                            <button type="submit" className="text-white bg-teal-900 px-8 text-sm py-2 rounded hover:bg-teal-950">
+                                <span className="d-inline">Login</span>
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            </Form>
-        </div>
+            </div>
+        </div >
     );
 } 
--- JWT-Frontend/src/pages/auth/Register.jsx
+++ JWT-Frontend/src/pages/auth/Register.jsx
@@ -1,17 +1,20 @@
-import { Form, Button, Alert } from "react-bootstrap";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlineArrowRight, AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 import { registerThunk, setError, setErrorMessage } from "../../redux/slice/userSlice";
 
 import { useDispatch, useSelector } from "react-redux";
+
+import registerSVG from "../../assets/register.svg";
+import { Alert } from "../../components/Alert";
 
 export default function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [verifyPassword, setVerify] = useState("");
     const [show, setShow] = useState(false);
-    const [validated, setValidated] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
     const error = useSelector(state => state.user.error);
     const isFetching = useSelector(state => state.user.isFetching);
@@ -39,15 +42,13 @@ export default function Register() {
 
         if ((email && password)) {
             if (password !== verifyPassword) {
-                setValidated(true);
                 setPassword("");
                 setVerify("");
                 return;
             }
 
             const response = await dispatch(registerThunk({ email, password }));
-            
-            if (errorMessage === null) {
+            if (response.payload?.status === 200) {
                 navigate("/login");
             }
 
@@ -56,62 +57,63 @@ export default function Register() {
             setVerify("");
         }
 
-        setValidated(true);
     };
 
     const handleChange = (val, setVal) => {
         setVal(val);
-        setValidated(true);
     };
 
+
     return (
-        <div className="w-100 d-flex justify-content-center align-items-center flex-column position-relative" style={{ height: "100vh" }}>
-            {
-                errorMessage && <Alert show={show} variant={"danger"} className="position-absolute top-0 start-50 translate-middle-x mt-4">
-                    {errorMessage}
-                </Alert>
-            }
-            <h2 className="mb-3" style={{ fontSize: "36px" }}>Signup</h2>
-            <Form className="w-25" onSubmit={handleSubmit} noValidate validated={validated}>
-                {/* email */}
-                <Form.Group controlId="formBasicEmail" className="mb-3">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" name="email"
-                        value={email} onChange={e => handleChange(e.target.value, setEmail)} required />
-                    <Form.Control.Feedback type="invalid">
-                        Please write your email
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-                {/* password */}
-                <Form.Group controlId="formBasicPassword" className="mb-3">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" name="password"
-                        value={password} onChange={e => handleChange(e.target.value, setPassword)} required />
-                    <Form.Control.Feedback type="invalid">
-                        Please write your password
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicVerifyPassword" className="mb-3">
-                    <Form.Label>Verify Password</Form.Label>
-                    <Form.Control type="password" placeholder="Verify Password" name="password"
-                        value={verifyPassword} onChange={e => handleChange(e.target.value, setVerify)} required />
-                    <Form.Control.Feedback type="invalid">
-                        Please verify your password
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-                <div className="text-center">
-                    <button type="button" className="btn btn-link" style={{ fontSize: "14px" }} onClick={() => navigate("/login")}>Go to login page</button>
+        <div style={{ height: "100vh"}} className="auth-page relative min-w-full flex justify-center items-center flex-col shadow-lg">
+            <Alert show={show} errorMessage={errorMessage} />
+            <div className=" flex bg-white px-12 py-20 rounded-lg">
+                <div className="mr-20">
+                    <img src={registerSVG} alt="register" className="w-80 h-full my-auto" />
                 </div>
-                {/* submit button */}
-                <div className="text-center mt-3">
-                    <Button variant="primary" type="submit">
-                        <span className="d-inline me-2">Register</span> { isFetching && <span className="spinner-border spinner-border-sm ms-2" role="status" aria-hidden="true"></span>}
-                    </Button>
+                <div className=" w-80">
+                    <h2 className="mb-4 font-semibold text-2xl text-teal-950">Register</h2>
+                    <form className="" onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <label className="block text-sm mb-1 text-slate-500">Email</label>
+                            <input type="email" placeholder="Enter email" name="email" className="text-teal-950 px-2 py-1 rounded-md border w-full border-gray-600 outline-0"
+                                value={email} onChange={e => handleChange(e.target.value, setEmail)} required />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="block text-sm mb-1 text-slate-500">Password</label>
+                            <div className="relative">
+                                <input type={showPassword ? "text" : "password"} placeholder="Password" name="password" className="text-teal-950 px-2 py-1 rounded-md border w-full border-gray-600 outline-0"
+                                    value={password} onChange={e => handleChange(e.target.value, setPassword)} required />
+                                <span className="absolute top-1/2 right-2 text-lg text-teal-800 transform -translate-y-1/2 cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
+                                    {showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="block text-sm mb-1 text-slate-500">Verify Password</label>
+                            <div className="relative">
+                                <input type={showPassword ? "text" : "password"} placeholder="Verify Password" name="password" className="text-teal-950 px-2 py-1 rounded-md border w-full border-gray-600 outline-0"
+                                    value={verifyPassword} onChange={e => handleChange(e.target.value, setVerify)} required />
+                                <span className="absolute top-1/2 right-2 text-lg text-teal-800 transform -translate-y-1/2 cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
+                                    {showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="text-center">
+                            <Link className="text-sm text-teal-900" to={"/login"}>Go to login page <AiOutlineArrowRight className="inline" /></Link>
+                        </div>
+                        {/* submit button */}
+                        <div className="text-center mt-8">
+                            <button type="submit" className="text-white bg-teal-900 px-8 text-sm py-2 rounded hover:bg-teal-950">
+                                <span className="d-inline">Register</span>
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            </Form>
+            </div>
         </div>
     )
 }
