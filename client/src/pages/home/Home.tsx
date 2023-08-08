ca6977b2a40f5c990515a3683c980717cc32246c
ogzCode <oguzguc44@gmail.com>
2023-08-08 08:50:59 +0300

    Added NoteSide component and subcomponents

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import SideMenu from "./SideMenu";
+import NoteSide from "./NoteSide";
 
 export default function Home() {
     return (
         <div className="min-h-screen grid grid-cols-12 ">
             <SideMenu/>
+            <NoteSide/>
         </div>
     );
 }
